c767c7bf62cff9adb35f00f64d1ecb8f98775ade
MigatteNoGokuii <anmar_2101@hotmail.com>
2022-11-14 15:28:22 +0400

    Update 1.1 - Programmed Algorithm Logic and UI

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,29 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Level : MonoBehaviour
 {
-    public int Req_CurrentLevel;
-    public float Req_Cuppuccino;
-    public float Req_SpendingOnPromotion;
-    public float Req_SalaryEmployee;
-    public float Req_Trainingspending;
 
-    public float currentYear;
+    public int TargetMoney;
 
-    public int CurrentLevel;
-    public float Cuppuccino;
-    public float SpendingOnPromotion;
-    public float SalaryEmployee;
-    public float Trainingspending;
+    public GameObject[] News;
+    public GameObject FailSucUI;
+    public TextMeshProUGUI SucFail;
+    public int currentYear = 1;
+    public int CollectedMoney;
+    public Button startYear;
+    public Button nextYear;
+    public Slider Cuppuccino;
+    public Slider SpendingOnPromotion;
+    public Slider SalaryEmployee;
+    public Slider Trainingspending;
+
+    bool yearEnded;
 
     void Start()
+    {
+        startYear.onClick.AddListener(() =>
+        {
+            CollectedMoney = algorithm((int)Cuppuccino.value, (int)SpendingOnPromotion.value, (int)SalaryEmployee.value, (int)Trainingspending.value);
+            if (CollectedMoney >= TargetMoney)
+            {
+                Success();
+            }
+            if (CollectedMoney < TargetMoney)
+            {
+                Failed();
+            }
+        });
+        
+        nextYear.onClick.AddListener(() =>
+        {
+            currentYear++;
+            News[currentYear - 1].gameObject.SetActive(true);
+            FailSucUI.gameObject.SetActive(false);
+        });
+    }
+
+    void Update()
     {
 
     }
 
-    void Update()
+    public int algorithm(int cuppuccinoPrice, int spendingOnPromo, int EmployeeSalary, int TrainSpending)
+    {
+        return cuppuccinoPrice * spendingOnPromo * EmployeeSalary * TrainSpending;
+    }
+
+    public void Success()
+    {
+        FailSucUI.gameObject.SetActive(true);
+        SucFail.text = "Succeeded"; 
+    }
+
+    public void Failed()
+    {
+        FailSucUI.gameObject.SetActive(true);
+        SucFail.text = "Failed";
+    }
+
+    public void nextLevel()
     {
 
     }
---
+++ Assets/Scripts/SliderValue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliderValue : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI value;
+    Slider myValue;
+
+    void Start()
+    {
+        myValue = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value.text = myValue.value.ToString();  
+    } 
+}
